ea182f4ba37b8369efcb17b54534264ed31ed358
oscar-company <oscararauzp@gmail.com>
2023-09-08 12:07:59 +0200

    Creación de clases para el agente

--- Doctorbot/Doctorbot.py
+++ Doctorbot/Doctorbot.py
@@ -27,14 +27,94 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 # Definimos las herramientas que el agente usará.
 search = DuckDuckGoSearchRun()
 
+#Definimos una herramienta de busqueda mas precisa.
+def duck_wrapper(input_text):
+    search_result = search.run(f'site:webmd.com {input_text}')
+    return search_result
+
 tools = [
     Tool(
-        name='Search',
-        func=search.run,
-        description='Herramienta muy util cuando tienes que responder preguntas sobre temas varios'
+      name='Search WebMD',
+        func=duck_wrapper,
+        description='Busqueda mas especifica en una web medica.'
     )
 ]
 
-obj = search.run('¿Cuales son los principales efectos de un ataque epileptico?')
+# Creamos el prompt template, en ingles para que la API lo entienda mejor.
+# Base template
+template = '''
+Answer the following questions as best as you can, bus speaking as compasionate medical professional. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now the final answer
+Final Answer : the final answer to the original input question
+
+Begin! Remember to answer as a compasionate medical professional when giving your final answer.
+
+Question: {input}
+{agent_scratchpad}
+'''
+
+# Clase para el Set up del template
+class CustomPromptTemplate(StringPromptTemplate):
+    template: str
+    tools: List[Tool]
+
+    def format(self, **kwargs) -> str:
+        # Se encarga de los 'intermediate steps' (AgentAction, Observation tuples) y los formatea de una manera particular.
+        intermediate_steps = kwargs.pop('intermediate_steps')
+        thoughts = ''
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f'\nObservation: {observation}\nThought: '
+        # Set the argent_scratchpad variable to that value
+        kwargs['agent_scratchpad'] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs['tools'] = '\n'.join([f'{tool.name}: {tool.description}' for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs['tool_name'] = ', '.join([tool.name for tool in self.tools])
+        return self.template.format(**kwargs)
+
+prompt = CustomPromptTemplate(
+    template=template,
+    tools=tools,
+    # Omite las variables 'agents_scratchpad', 'tools' and 'tools_name' porque son generadas dinamicamente en la función superior.
+    # Incluir las variables 'input' e 'intermediate_steps' porque son necesarias.
+    input_variable=['input', ' intermediate_steps']
+)
+
+class CustomOutputParser(AgentOutputParser):
+    def pase(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check si ha terminado el agente
+        if 'Final Answer: ' in llm_output:
+            return AgentFinish(
+                #Return de valor en diccionario con key 'output'
+                return_values={'output': llm_output.split('Final Answer:')[-1].strip()},
+                log=llm_output,
+            )
+        regex = r'Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)'
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: '{llm_output}'")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+
+        # Return de action y action_input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+output_parser = CustomOutputParser
+
+# Setup agent
+
+
+obj = search.run('sire:webmd.com ¿Cuales son los principales efectos de un ataque epileptico?')
 
 print(obj)
