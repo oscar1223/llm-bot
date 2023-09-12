05202174d947e3751628f0337494f7e7580c2153
oscar-company <oscararauzp@gmail.com>
2023-09-12 10:19:18 +0200

    Load_qa_chain

--- chatpdf/pdfprocessing.py
+++ chatpdf/pdfprocessing.py
@@ -1,4 +1,5 @@
 from langchain.chains import RetrievalQA
+from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -34,36 +35,36 @@ Helpful Answer:
 QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
 loader = PyPDFLoader('./pdfs/napoleon.pdf')
-data = loader.load_and_split()
-index = pinecone.Index('https://qaindex-92149fa.svc.gcp-starter.pinecone.io')
+data = loader.load()
+#index = pinecone.Index('https://qaindex-92149fa.svc.gcp-starter.pinecone.io')
 
-#pdf_splitter = RecursiveCharacterTextSplitter()
-#all_splits = pdf_splitter.split_documents(data)
+pdf_splitter = RecursiveCharacterTextSplitter()
+all_splits = pdf_splitter.split_documents(data)
+
+
 
 
 #vectorstore = Chroma.from_documents(documents=data, embedding=OpenAIEmbeddings())
 #retriever = VectorStoreRetriever(vectorstore=vectorstore)
-#faiss_index = FAISS.from_documents(data, OpenAIEmbeddings())
+faiss_index = FAISS.from_documents(data, OpenAIEmbeddings())
 
-pinecone.init(api_key='55b25d59-23c8-466a-968e-a4d2d690f09a', enviroment='gcp-starter')
+#pinecone.init(api_key='55b25d59-23c8-466a-968e-a4d2d690f09a', enviroment='gcp-starter')
 
 embedding = OpenAIEmbeddings()
-vectordb = Pinecone.from_documents(data, embedding, index_name=index)
+#vectordb = Pinecone.from_documents(data, embedding, index_name=index)
 
 
 question = '¿Donde nacio Napoleon?'
 
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0, verbose=True)
 
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    return_source_documents=True,
-    retriever=vectordb,
-    chain_type_kwargs={'prompt': QA_CHAIN_PROMPT},
-    verbose=True
-)
-
-result = qa_chain({'query':question})
+qa_chain = load_qa_chain(llm=llm,
+                         chain_type='stuff',
+                         prompt=QA_CHAIN_PROMPT,
+                         verbose=True
+                         )
+docs = faiss_index.similarity_search(question, include_metadata=True)
+result = qa_chain.run(input_documents=docs, question=question, return_only_outputs=True, verbose=True)
 print(result)
 
 
