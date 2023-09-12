19e0541fd4d498e58ef07a7cc8fe8ff1e76ecbe9
oscar-company <oscararauzp@gmail.com>
2023-09-12 13:41:26 +0200

    Page stuff
    La pagina se muestra bien, pero varia en función a como hagas la pregunta.

--- chatpdf/pdfprocessing.py
+++ chatpdf/pdfprocessing.py
@@ -1,3 +1,4 @@
+from langchain import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
@@ -16,13 +17,12 @@ from langchain.vectorstores.base import VectorStoreRetriever
 # read local .env file
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
-pinecone_api_key = pinecone.api_key = os.environ['PINECONE_API_KEY']
 
 #Leemos el documento
 template='''
 Use the following pieces of context to answer the question at the end. 
 If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-Use 150 word maximun to answer.
+Use 150 word maximum to answer.
 Always include the page where the answer is.
 Always say "thanks for asking!" at the end of the answer.
 Context is delimited by triple dollar signs.
@@ -36,35 +36,43 @@ QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
 loader = PyPDFLoader('./pdfs/napoleon.pdf')
 data = loader.load()
-#index = pinecone.Index('https://qaindex-92149fa.svc.gcp-starter.pinecone.io')
 
-pdf_splitter = RecursiveCharacterTextSplitter()
+pdf_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1500,
+    chunk_overlap=150,
+    separators=["\n\n", "\n", "(?<=\. )", " ", ""]
+)
 all_splits = pdf_splitter.split_documents(data)
 
+persist_directory = './pdfs/chroma/'
 
+vectorstore = Chroma.from_documents(documents=data,
+                                    embedding=OpenAIEmbeddings(),
+                                    persist_directory=persist_directory)
 
+retriever = VectorStoreRetriever(vectorstore=vectorstore)
 
-#vectorstore = Chroma.from_documents(documents=data, embedding=OpenAIEmbeddings())
-#retriever = VectorStoreRetriever(vectorstore=vectorstore)
-faiss_index = FAISS.from_documents(data, OpenAIEmbeddings())
-
-#pinecone.init(api_key='55b25d59-23c8-466a-968e-a4d2d690f09a', enviroment='gcp-starter')
+#faiss_index = FAISS.from_documents(data, OpenAIEmbeddings())
 
 embedding = OpenAIEmbeddings()
-#vectordb = Pinecone.from_documents(data, embedding, index_name=index)
 
-
-question = '¿Donde nacio Napoleon?'
+question = '¿A que población pertenecia Napoleón?'
+docs = vectorstore.similarity_search(query=question, k=5, include_metadata=True)
+#Mostramos los metadatos de los chucnks selecionados.
+for doc in docs:
+    print(doc)
 
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0, verbose=True)
 
-qa_chain = load_qa_chain(llm=llm,
-                         chain_type='stuff',
-                         prompt=QA_CHAIN_PROMPT,
-                         verbose=True
-                         )
-docs = faiss_index.similarity_search(question, include_metadata=True)
-result = qa_chain.run(input_documents=docs, question=question, return_only_outputs=True, verbose=True)
+qa_chain = RetrievalQA.from_chain_type(llm,
+                                       retriever=retriever,
+                                       chain_type='stuff',
+                                       chain_type_kwargs={'prompt': QA_CHAIN_PROMPT},
+                                       verbose=True
+                                       )
+
+
+result = qa_chain({'query': question, 'input_documents': docs, 'return_only_outputs': False, 'verbose': True})
 print(result)
 
 
