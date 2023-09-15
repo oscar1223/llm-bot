4d03d165de2ff61e2be3e79fb6bb96e247b84bdf
oscar-company <oscararauzp@gmail.com>
2023-09-15 14:28:25 +0200

    Agent first try
    Bad connection with DB, look at uri.

--- SQLconverter/llmsql.py
+++ SQLconverter/llmsql.py
@@ -8,10 +8,21 @@ from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
 from langchain.llms import OpenAI
 
-
 # read local .env file
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
 
+# Langchain to Database connection
+llm_db = SQLDatabase.from_uri('C:/Users/oarauz/musicdb.db')
 
-#Database stuff
+toolkit = SQLDatabaseToolkit(db=llm_db, llm=OpenAI(temperature=0))
+
+# Instanciamos agent
+agent_executor = create_sql_agent(
+    llm=OpenAI(temperature=0),
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+)
+
+agent_executor.run('Muestrame todos los registros de la tabla Album')
